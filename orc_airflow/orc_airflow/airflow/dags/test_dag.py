5465222d9ceaecf993af7b8c37d191bb2530c548
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2025-07-08 12:00:37 +1000

    adds orc_airflow and basic airflow dag structure

---
+++ orc_airflow/orc_airflow/airflow/dags/test_dag.py
@@ -0,0 +1,11 @@
+from airflow.sdk import task, dag
+
+
+@dag(dag_id="test_dag", description="a test dag to check if airflow itself is working")
+def test_dag():
+    @task
+    def test_task():
+        return "Hello World"
+
+
+test_dag()
---
+++ orc_airflow/orc_airflow/definitions.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+DB_NAME = "data_mining.db"
+DB_PATH = BASE_DIR / DB_NAME
+RESOURCES = BASE_DIR / "resources"
