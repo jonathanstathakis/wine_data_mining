eb10b9fc1c3c11da8f38e85e397aedb39a2fbfbd
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2025-07-10 12:55:05 +1000

    sets up db connection through hook

--- orc_airflow/orc_airflow/airflow/dags/wine_list_etl.py
+++ orc_airflow/orc_airflow/airflow/dags/wine_list_etl.py
@@ -1,6 +1,7 @@
 from airflow.sdk import task, dag
 import duckdb as db
 from orc_airflow.definitions import DB_PATH, RESOURCES
+from duckdb_provider.hooks.duckdb_hook import DuckDBHook
 
 import logging
 
@@ -11,12 +12,30 @@ logger = logging.getLogger(__name__)
 def dag_wine_list_etl():
     @task
     def run_wine_list_etl():
+        duckdb_conn_id = "data_mining_db"
         from wine_list_etl.etl import run_etl
+
+        duckdb_hook = DuckDBHook(duckdb_conn_id=duckdb_conn_id)
+        conn = duckdb_hook.get_conn()
 
         pdf_path = RESOURCES / "bennelong_wine_list.pdf"
         page_range = (6, -1)
+        run_etl(conn=conn, pdf_path=pdf_path, page_range=page_range)
+
+    @task
+    def load_wine_list():
+        """
+        load the wine_list table into another table of the same name
+        with the merged text as a primary key, a run_id and run_date.
+        """
         with db.connect(DB_PATH) as conn:
-            run_etl(conn=conn, pdf_path=pdf_path, page_range=page_range)
+            query = """
+            create table wine_list (
+            line_num int,
+            page_num int,
+
+            )
+            """
 
     run_wine_list_etl()
 
