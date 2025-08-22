2d89d201d13a45d407c7315f450c090fc3057ead
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2025-08-23 01:23:56 +1000

    add primary key and clear definition of aggregated

--- orc_airflow/orc_airflow/airflow/include/wine_list_etl/aggregate_lines.sql
+++ orc_airflow/orc_airflow/airflow/include/wine_list_etl/aggregate_lines.sql
@@ -1,19 +1,33 @@
-create or replace table aggregated as (
-    select
-        line_num,
-        page_num,
-        ROW_NUMBER()
-            over (
-                order by page_num asc, line_num asc
-            )
-            as line_num_tot,
-        STRING_AGG(text, ' ') as merged_text,
-        ARRAY_AGG(TO_JSON(line_numbered_pages)) as word_json
-    from
-        line_numbered_pages
-    group by
-        line_num,
-        page_num
-    order by
-        line_num_tot
+create or replace table aggregated (
+    line_num int,
+    page_num int,
+    line_num_tot int primary key,
+    merged_text varchar,
+    word_json json
 );
+
+insert into aggregated (
+    line_num,
+    page_num,
+    line_num_tot,
+    merged_text,
+    word_json
+)
+select
+    line_num,
+    page_num,
+    ROW_NUMBER()
+        over (
+            order by page_num asc, line_num asc
+        )
+        as line_num_tot,
+    STRING_AGG(text, ' ') as merged_text,
+    ARRAY_AGG(TO_JSON(line_numbered_pages)) as word_json
+from
+    line_numbered_pages
+group by
+    line_num,
+    page_num
+order by
+    line_num_tot
+;
