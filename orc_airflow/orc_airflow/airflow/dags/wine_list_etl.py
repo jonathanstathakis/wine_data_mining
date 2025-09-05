738dc94f260412e0953e49db61c2133bdd1dc73c
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2025-09-06 01:15:54 +1000

    adds sectionPath and sectionpath_id to pageLine

--- orc_airflow/orc_airflow/airflow/dags/wine_list_etl.py
+++ orc_airflow/orc_airflow/airflow/dags/wine_list_etl.py
@@ -180,7 +180,7 @@ def wine_etl_transform_section_path():
     duckdb_conn_id = "wine_list_etl_transform"
     (
         SQLExecuteQueryOperator(
-            task_id="create_sectionPath_dim_tbl",
+            task_id="create_sectionPath_dim_tbl_insert_into_pageline",
             conn_id=duckdb_conn_id,
             sql="create_insert_section_path.sql",
             autocommit=True,
--- orc_airflow/orc_airflow/airflow/include/wine_list_etl_transform_create_section_path/create_insert_section_path.sql
+++ orc_airflow/orc_airflow/airflow/include/wine_list_etl_transform_create_section_path/create_insert_section_path.sql
@@ -1,15 +1,75 @@
-begin;
 create or replace sequence SECTIONPATH_SEQ;
 
 create or replace table SECTIONPATH (
-    ID int primary key default nextval('sectionPath_seq'),
+    ID int primary key default nextval('SECTIONPATH_SEQ'),
     PATH varchar not null unique
 );
 
+
+
+create or replace table sectionPathLoading (
+line_id int unique not null,
+line_num_tot int unique not null,
+path varchar,
+path_id int
+);
+
+insert into sectionPathLoading (
+  line_id,
+  line_num_tot,
+  path
+)
+select
+    line_id,
+    line_num_tot,
+    array_to_string([SECTION, SUBSECTION, SUBSUBSECTION], '/').replace('//','/') as path
+from SECTIONLABELSBYLINE
+order by
+  line_num_tot;
+
 insert into sectionPath (path)
 select
-    distinct array_to_string([SECTION, SUBSECTION, SUBSUBSECTION], '/').replace('//','/') as path
-from SECTIONLABELSBYLINE;
+    distinct path
+from
+    sectionPathLoading;
 
-select * from sectionPath;
-rollback;
+update sectionPathLoading s
+set
+  path_id = q.path_id
+from (
+select 
+  l.line_id as line_id,
+  l.path as left_path,
+  p.path as right_path,
+  p.id as path_id
+from
+  sectionPathLoading l
+left join
+  sectionPath p
+on
+  l.path = p.path
+order by
+  l.line_num_tot
+) q
+where
+s.line_id = q.line_id
+;
+
+-- select * from sectionPathLoading;
+update pageline p
+set
+    sectionpath_id = l.path_id
+from
+    sectionPathLoading l
+where
+    l.line_id = p.id;
+
+select 
+  * 
+from 
+  pageLine  l
+join
+  sectionPath s
+on
+  l.sectionpath_id = s.id
+order by line_num_tot;
