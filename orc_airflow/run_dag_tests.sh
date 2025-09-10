40bc026b3fda8ebc985ace99f4583438e31adc39
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2025-09-10 14:53:06 +1000

    uncomments run_dag_tests

--- orc_airflow/run_dag_tests.sh
+++ orc_airflow/run_dag_tests.sh
@@ -4,9 +4,9 @@
 file='/Users/jonathan/jonathan/projects/wine_wiki/wine_data_mining/orc_airflow/orc_airflow/airflow/include/test.csv'
 
 rm $file
-# airflow dags test wine_list_etl_extract &&
-#   airflow dags test wine_list_etl_tform &&
-#   airflow dags test wine_list_etl_tform_section_label &&
-#   airflow dags test wine_list_etl_tform_col_decomp &&
-airflow dags test wine_list_etl_load &&
+airflow dags test wine_list_etl_extract &&
+  airflow dags test wine_list_etl_tform &&
+  airflow dags test wine_list_etl_tform_section_label &&
+  airflow dags test wine_list_etl_tform_col_decomp &&
+  airflow dags test wine_list_etl_load &&
   open $file
